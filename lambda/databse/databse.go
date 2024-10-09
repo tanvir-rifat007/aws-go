5126eb0fe6f6ee812ebf0390ffdac02958e9fd0c
tanvir07-cell <tr9836859@gmail.com>
2024-10-09 06:35:34 -0700

    Chore:for the aws and using the dynamodb databse

--- lambda/databse/databse.go
+++ lambda/databse/databse.go
@@ -3,9 +3,13 @@
 package databse
 
 import (
+	"awsgo.tanvirrifat.io/types"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
+
+const TABLE_NAME = "userTable"
 
 type DynamoDBClient struct{
 
@@ -17,4 +21,63 @@ func NewDynamoDBClient() DynamoDBClient{
 		databaseStore: dynamodb.New(session.Must(session.NewSession())),
 		
 	}
+}
+
+
+func (u DynamoDBClient) IsUserExists(username string) (bool,error){
+
+	result,err:= u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username":{
+				 S: aws.String(username),
+			},
+		},
+
+	})
+
+	// if user exist
+	// but some aws error occured
+
+	if err!=nil{
+		return true,err
+	}
+
+	// if user doesn't exist
+
+	if result.Item==nil{
+		return false,nil
+	}
+
+	// if user exist
+
+	return true,nil
+
+}
+
+
+func (u DynamoDBClient) AddUser(user types.Register) error{
+	
+	 item:= &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"username":{
+				S: aws.String(user.Username),
+			},
+			"password":{
+				S: aws.String(user.Password),
+			},
+		},
+	 }
+
+	_, err:= u.databaseStore.PutItem(item)
+
+
+	if err!=nil{
+		return err
+	}
+
+	return nil
+
+
 }
